13877a8d3ec874bd33f7b4f7f3bfc4c282d53cf5
alenzer <alenzer0902@gmail.com>
2022-04-14 03:00:34 -0700

    max width font

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo} from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { QueryClient, QueryClientProvider, useInfiniteQuery } from "react-query"
 import { Outlet, Link } from "react-router-dom";
 import { VStack, Flex, useDisclosure, useEventListenerMap } from '@chakra-ui/react'
@@ -9,52 +9,58 @@ import Footer from "./Pages/Footer";
 import DepositModal from './Pages/DepositModal'
 import WithdrawModal from './Pages/WithdrawModal'
 import WaitingModal from './Pages/WaitingModal';
-import {fetchData, checkNetwork} from './Util';
+import { fetchData, checkNetwork } from './Util';
 
 const Layout = () => {
   const networkName = useNetworkName();
   const queryClient = useQueryClient();
-  
+
   const { isOpen: isOpenDeposit, onOpen: onOpenDeposit, onClose: onCloseDeposit } = useDisclosure();
   const { isOpen: isOpenWithdraw, onOpen: onOpenWithdraw, onClose: onCloseWithdraw } = useDisclosure();
   const { isOpen: isOpenWaiting, onOpen: onOpenWaiting, onClose: onCloseWaiting } = useDisclosure();
-  
-  const {state, dispatch} = useStore();
-  const lcd =  useLCD();
+
+  const { state, dispatch } = useStore();
+  const lcd = useLCD();
   const wallet = useWallet();
 
-  useEffect( () => {
-    dispatch({type: ActionKind.setOpenDepositModal, payload: onOpenDeposit});
-    dispatch({type: ActionKind.setOpenWithdrawModal, payload: onOpenWithdraw});
-    dispatch({type: ActionKind.setOpenWaitingModal, payload: onOpenWaiting});
-    dispatch({type: ActionKind.setCloseWaitingModal, payload: onCloseWaiting});
+  useEffect(() => {
+    dispatch({ type: ActionKind.setOpenDepositModal, payload: onOpenDeposit });
+    dispatch({ type: ActionKind.setOpenWithdrawModal, payload: onOpenWithdraw });
+    dispatch({ type: ActionKind.setOpenWaitingModal, payload: onOpenWaiting });
+    dispatch({ type: ActionKind.setCloseWaitingModal, payload: onCloseWaiting });
   }, [dispatch, onOpenDeposit, onOpenWithdraw, onOpenWaiting, onCloseWaiting])
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchAll = async () => {
       fetchData(state, dispatch)
     }
-    if( checkNetwork(wallet, state) )
+    if (checkNetwork(wallet, state))
       fetchAll()
   }, [lcd, wallet])
 
   return (
     <QueryClientProvider client={queryClient} key={networkName}>
-      <VStack
+      <Flex
         background={'black'}
-        fontFamily={'SF-Pro-Text'}
-        letterSpacing={'-0.06em'}
-        color={'white'}
-        spacing={'10px'}
-        // onClick={() => onOpenWaiting()}
+        justify={'center'}
+        w={'100%'}
       >
-        <Navbar />
-        <Outlet />
-        <Footer />
-        <DepositModal isOpen={isOpenDeposit} onClose={onCloseDeposit} />
-        <WithdrawModal isOpen={isOpenWithdraw} onClose={onCloseWithdraw} />
-        <WaitingModal isOpen={isOpenWaiting} onClose={onCloseWaiting} />
-      </VStack>
+        <VStack
+          fontFamily={'SF-Pro-Text'}
+          letterSpacing={'-0.06em'}
+          spacing={'10px'}
+          color={'white'}
+          maxWidth={'1440px'}
+          w = {'100%'}
+        >
+          <Navbar />
+          <Outlet />
+          <Footer />
+          <DepositModal isOpen={isOpenDeposit} onClose={onCloseDeposit} />
+          <WithdrawModal isOpen={isOpenWithdraw} onClose={onCloseWithdraw} />
+          <WaitingModal isOpen={isOpenWaiting} onClose={onCloseWaiting} />
+        </VStack>
+      </Flex>
     </QueryClientProvider>
   )
 };
