c1bf8897a315fc250d5b4a803440c142ce18994f
alenzer <alenzer0902@gmail.com>
2022-04-15 17:41:34 -0700

    fix font

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -46,7 +46,7 @@ const Layout = () => {
         w={'100%'}
       >
         <VStack
-          fontFamily={'SF-Pro'}
+          fontFamily={'SF-Pro-Text'}
           letterSpacing={'-0.06em'}
           spacing={'10px'}
           color={'white'}
